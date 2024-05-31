4e127933eb74941b248fc6a4fbd4b2c99397f35a
Semen Ganiev <sganiev@2-D-4.42heilbronn.de>
2024-05-31 18:31:18 +0200

    Moved checking of no parameters to the top of main

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 18:38:00 by sganiev           #+#    #+#             */
-/*   Updated: 2024/05/31 13:26:54 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/05/31 18:28:37 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,23 @@ int	main(int argc, char **argv)
 {
 	t_sort	data;
 
-	if (argc > 1)
+	if (argc == 1 || (argc == 2 && (argv[1][0] == '\0')))
+		return (1);
+	data.err_flag = 0;
+	data.is_malloc = 0;
+	if (argc == 2)
 	{
-		data.err_flag = 0;
-		data.is_malloc = 0;
-		if (argc == 2)
-		{
-			data.is_malloc = 1;
-			data.str_nums = ft_split(argv[1], ' ');
-			data.err_flag = check_nums(data.str_nums);
-		}
-		else
-			data.err_flag = check_nums(argv + 1);
-		check_err_flag(&data);
-		fill_a_stack(&data);
-		print_sort_commands(&data);
-		return (0);
+		data.is_malloc = 1;
+		data.str_nums = ft_split(argv[1], ' ');
+		data.err_flag = check_nums(data.str_nums);
 	}
+	else
+	{
+		data.str_nums = argv + 1;
+		data.err_flag = check_nums(data.str_nums);
+	}
+	check_err_flag(&data);
+	fill_a_stack(&data);
+	print_sort_commands(&data);
 	return (0);
 }
