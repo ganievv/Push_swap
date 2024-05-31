2ba83bca15a350e93d00184178e0fdc15a6f1809
Semen Ganiev <sganiev@2-D-4.42heilbronn.de>
2024-05-31 14:22:10 +0200

    Added checking if the numbers is 'int'

--- check_nums.c
+++ check_nums.c
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 13:30:42 by sganiev           #+#    #+#             */
-/*   Updated: 2024/05/31 13:43:59 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/05/31 14:18:13 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 static int	check_str(char *str)
 {
-	int	i;
+	int		i;
+	long	num;
 
 	i = 0;
-	if (str[i] == '-')
+	if (str[i] == '-' || str[i] == '+')
 		i++;
 	while (str[i])
 	{
@@ -26,21 +27,22 @@ static int	check_str(char *str)
 		else
 			return (1);
 	}
+	num = ft_atol(str);
+	if (num < INT_MIN || num > INT_MAX)
+		return (1);
 	return (0);
 }
 
 int	check_nums(char **arr)
 {
-	int	err_flag;
 	int	i;
 
 	i = 0;
-	err_flag = 0;
 	while (arr[i])
 	{
-		err_flag = check_str(arr[i]);
-		if (err_flag == 1)
+		if (check_str(arr[i]) == 1)
 			return (1);
 		i++;
 	}
+	return (0);
 }
