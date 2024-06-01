0f7e09dfaa9631a0532e58d246e947ecf921b115
Semen Ganiev <sganiev@2-D-2.42heilbronn.de>
2024-06-01 15:53:19 +0200

    Added output to standart error

--- check_err_flag.c
+++ check_err_flag.c
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 14:24:49 by sganiev           #+#    #+#             */
-/*   Updated: 2024/05/31 14:29:43 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/05/31 18:37:46 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	check_err_flag(t_sort *data)
 	if (data->err_flag == 1)
 	{
 		if (data->is_malloc == 1)
-			clean_arr_ptrs(data->str_nums);
-		ft_printf("Error\n");
+			clean_arr_ptrs(data);
+		write(STDERR_FILENO, "Error\n", 6);
 		exit(1);
 	}
 }
