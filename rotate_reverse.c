45736eba7ea7b683f590558499a606f8fa64993a
Semen Ganiev <sganiev@2-D-2.42heilbronn.de>
2024-06-01 13:33:23 +0200

    Added reverse rotate function

---
+++ rotate_reverse.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate_reverse.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/31 21:42:54 by sganiev           #+#    #+#             */
+/*   Updated: 2024/06/01 13:31:59 by sganiev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	rotate_reverse(t_list **stack)
+{
+	t_list	*first;
+	t_list	*last;
+
+	if (stack && *stack && (*stack)->next)
+	{
+		last = find_last_node(*stack);
+		first = *stack;
+		last->prev->next = NULL;
+		last->prev = NULL;
+		last->next = first;
+		first->prev = last;
+		*stack = last;
+	}
+}
