224cfc21521a26305dde2f9e60bb774efbcca9bc
Semen Ganiev <sganiev@2-D-2.42heilbronn.de>
2024-06-03 15:45:41 +0200

    Added printing of linked list, putting indexes, its freeing

--- print_sort_commands.c
+++ print_sort_commands.c
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 17:50:12 by sganiev           #+#    #+#             */
-/*   Updated: 2024/05/31 19:10:49 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/06/03 15:38:53 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,29 @@ static void	num_counter(t_sort *data)
 	}
 }
 
+static void	print_a_stack(t_list *stack)
+{
+	ft_printf("a_stack: ");
+	while (stack)
+	{
+		ft_printf("%d ", stack->num);
+		stack = stack->next;
+	}
+	ft_printf("\n");
+}
+
 void	print_sort_commands(t_sort *data)
 {
 	data->num_count = 0;
 	num_counter(data);
+	put_index(&(data->a_stack));
+	if (is_sorted(data) == 0)
+	{
+		if (data->num_count < 4)
+			sort_less_three(data);
+		else if (data->num_count > 3)
+			sort_large_amount(data);
+	}
+	print_a_stack(data->a_stack);
+	free_lists(data);
 }
