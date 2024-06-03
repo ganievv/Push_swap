456e0c8f0faffc52534bf6d8b8f0024e559676a7
Semen Ganiev <sganiev@2-D-2.42heilbronn.de>
2024-06-03 15:58:15 +0200

    Added case for 3 nums when max is on the second position

--- sort_less_three.c
+++ sort_less_three.c
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 13:43:37 by sganiev           #+#    #+#             */
-/*   Updated: 2024/06/01 15:44:30 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/06/03 14:23:41 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,9 @@ static	void	three_num(t_sort *data)
 	if ((current->num > current->next->num)
 		&& (current->num > current->next->next->num))
 		rotate_norm(&(data->a_stack));
+	else if ((current->next->num > current->num)
+		&& (current->next->num > current->next->next->num))
+		rotate_reverse(&(data->a_stack));
 	current = data->a_stack;
 	if (current->num > current->next->num)
 		swap(data->a_stack, data->num_count);
