f752b94292d79b9eeeec7ee88999d7ae51d2de62
Semen Ganiev <sganiev@2-D-2.42heilbronn.de>
2024-06-03 18:23:51 +0200

    Created function for finding min value in linked list

---
+++ find_min.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   find_min.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 18:16:39 by sganiev           #+#    #+#             */
+/*   Updated: 2024/06/03 18:20:34 by sganiev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_list	*find_min(t_list *stack)
+{
+	t_list	*min;
+
+	if (stack == NULL)
+		return (NULL);
+	min = stack;
+	while (stack)
+	{
+		if (stack->num < min->num)
+			min = stack;
+		stack = stack->next;
+	}
+	return (min);
+}
