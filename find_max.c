d2b6825dc9241cde737944a416ffa8888f3378bc
Semen Ganiev <sganiev@2-D-2.42heilbronn.de>
2024-06-03 16:29:57 +0200

    Created function for finding max node in stack

---
+++ find_max.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   find_max.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 16:04:23 by sganiev           #+#    #+#             */
+/*   Updated: 2024/06/03 16:13:04 by sganiev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_list	*find_max(t_list *stack)
+{
+	t_list	*max;
+
+	if (stack == NULL)
+		return (NULL);
+	max = stack;
+	while (stack)
+	{
+		if (max->num < stack->num)
+			max = stack;
+		stack = stack->next;
+	}
+	return (max);
+}
