ab83b74d829eb6e0634c7893912bfc7bfbd1553d
Semen Ganiev <sganiev@2-B-2.42heilbronn.de>
2024-06-04 15:01:05 +0200

    Created searching of index in 'b' stack to insert num

--- sort_large_amount.c
+++ sort_large_amount.c
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 19:56:22 by sganiev           #+#    #+#             */
-/*   Updated: 2024/06/03 18:25:48 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/06/04 14:59:14 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,21 +47,66 @@ static void	push_top_two(t_sort *data)
 //		current_a->steps_count++;
 //}
 
+// are you changing b_stack when you connect end node with start ?
+
+static int	find_insert_position_b(t_sort *data, int nbr)
+{
+	t_list	*current_b;
+	t_list	*max_b;
+	t_list	*min_b;
+	t_list	*next_b;
+
+	max_b = find_max(data->b_stack);
+	min_b = find_min(data->b_stack);
+	current_b = max_b;
+	while (1)
+	{
+		if (current_b->next == NULL)
+			next_b = data->b_stack;
+		else
+			next_b = current_b->next;
+		if ((current_b->num > nbr)
+			&& (next_b->num < nbr))
+		{
+			return (next_b->index);
+		}
+		current_b = next_b;
+		if (current_b->num == min_b->num)
+			return (-1);
+	}
+}
+
+static void	choose_push_num(t_sort *data)
+{
+	t_list	*current_a;
+	t_list	*current_b;
+	int		rotate_a;
+	int		rotate_b;
+
+	rotate_a = 0;
+	rotate_b = 0;
+	current_a = data->a_stack;
+	current_b = data->b_stack;
+	while (current_a)
+	{
+		if ((current_a->index + 1) <= data->num_count / 2)
+			rotate_a += current_a->index;
+		else
+			rotate_a += (data->num_count - current_a->index);
+		current_a = current_a->next;
+	}
+}
+
 void	sort_large_amount(t_sort *data)
 {
 	t_list	*current_a;
-	t_list	*cheapest;
-	t_list	*max_b;
 
 	push_top_two(data);
 	steps_initialize(data);
 	current_a = data->a_stack;
-	while (current_a)
+	while (data->num_count > 3)
 	{
-		if ((current_a->index + 1) <= data->num_count / 2)
-			current_a->steps_count += current_a->index;
-		else
-			current_a->steps_count += (data->num_count - current_a->index);
-		current_a = current_a->next;
+		choose_push_num(data);
+		data->num_count--;
 	}
 }
