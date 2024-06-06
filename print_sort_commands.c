dc1b77f040ca3437fa2873074906d726d477336a
Semen Ganiev <sganiev@3-B-1.42heilbronn.de>
2024-06-06 20:05:03 +0200

    Commited lnes for stacks printing

--- print_sort_commands.c
+++ print_sort_commands.c
@@ -6,33 +6,33 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 17:50:12 by sganiev           #+#    #+#             */
-/*   Updated: 2024/06/06 15:50:06 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/06/06 19:52:47 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	print_a_stack(t_list *stack)
-{
-	ft_printf("a_stack: ");
-	while (stack)
-	{
-		ft_printf("%d ", stack->num);
-		stack = stack->next;
-	}
-	ft_printf("\n");
-}
+//static void	print_a_stack(t_list *stack)
+//{
+//	ft_printf("a_stack: ");
+//	while (stack)
+//	{
+//		ft_printf("%d ", stack->num);
+//		stack = stack->next;
+//	}
+//	ft_printf("\n");
+//}
 
-static void	print_b_stack(t_list *stack)
-{
-	ft_printf("b_stack: ");
-	while (stack)
-	{
-		ft_printf("%d ", stack->num);
-		stack = stack->next;
-	}
-	ft_printf("\n");
-}
+//static void	print_b_stack(t_list *stack)
+//{
+//	ft_printf("b_stack: ");
+//	while (stack)
+//	{
+//		ft_printf("%d ", stack->num);
+//		stack = stack->next;
+//	}
+//	ft_printf("\n");
+//}
 
 void	print_sort_commands(t_sort *data)
 {
@@ -45,7 +45,7 @@ void	print_sort_commands(t_sort *data)
 		else if (data->num_count_a > 3)
 			sort_large_amount(data);
 	}
-	print_a_stack(data->a_stack);
-	print_b_stack(data->b_stack);
+	//print_a_stack(data->a_stack);
+	//print_b_stack(data->b_stack);
 	free_lists(data);
 }
